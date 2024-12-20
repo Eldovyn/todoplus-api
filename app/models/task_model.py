50e9163ee4543fd4354e305bbe606877608eabf0
Eldovyn <farras.pramudita@gmail.com>
2024-12-20 20:39:57 +0700

    feat(is_favorite): add column is_favorite

--- app/models/task_model.py
+++ app/models/task_model.py
@@ -6,6 +6,7 @@ class TaskModel(me.Document):
     user = me.ReferenceField(UserModel, required=True, reverse_delete_rule=me.CASCADE)
     title = me.StringField(required=True)
     is_completed = me.BooleanField(required=True, default=False)
+    is_favorite = me.BooleanField(required=True, default=False)
     created_at = me.IntField(required=True)
 
     meta = {"collection": "tasks"}
